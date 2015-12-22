bd6d2b3b0c0344a0063695f230088a367efa683f
Bucky <buckyroberts@gmail.com>
2015-12-22 07:12:20 -0500

    Working on sniffer.

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
 
 
-HOST = '127.0.0.1'
+HOST = '192.168.0.5'
 PORT = 9999
 BUF_SIZE = 20480
 
@@ -23,21 +23,19 @@ def socket_connect():
         print("Socket connection error: " + str(err))
 
 
-def send_request():
+# Receive messages from remote server and run on local machine
+def receive_messages():
     while True:
-        data = 'GET / HTTP/1.0\r\n\r\n'
-        s.send(data.encode('utf-8'))
         data = s.recv(BUF_SIZE)
-        if not data:
-            break
-        print(data.decode('utf-8'))
+        print('Server> ' + data[:].decode("utf-8"))
+        s.send(str.encode('Client> ok'))
     s.close()
 
 
 def main():
     socket_create()
     socket_connect()
-    send_request()
+    receive_messages()
 
 
 main()
--- server.py
+++ server.py
@@ -9,8 +9,8 @@ BUF_SIZE = 20480
 # Create a socket
 def socket_create():
     try:
-        global s
-        s = socket.socket()
+        global server_socket
+        server_socket = socket.socket()
     except socket.error as err:
         print("Could not create socket: " + str(err))
 
@@ -18,17 +18,33 @@ def socket_create():
 # Bind socket to port and wait for connections
 def socket_bind():
     try:
-        s.bind((HOST, PORT))
-        s.listen(5)
+        server_socket.bind((HOST, PORT))
+        server_socket.listen(5)
+        print("Server waiting for connection...")
     except socket.error as err:
         print("Socket binding error: " + str(err))
 
 
 # Accept connection from a client
 def socket_accept():
-    conn, address = s.accept()
+    client_socket, address = server_socket.accept()
     print("Connection has been established | " + "IP " + address[0] + " | Port " + str(address[1]))
-    conn.close()
+    send_commands(client_socket)
+    client_socket.close()
+
+
+# Send commands
+def send_commands(client_socket):
+    while True:
+        msg = input('Server> ')
+        if msg == 'quit':
+            client_socket.close()
+            server_socket.close()
+            break
+        else:
+            client_socket.send(str.encode(msg))
+            client_response = str(client_socket.recv(BUF_SIZE), "utf-8")
+            print(client_response)
 
 
 if __name__ == '__main__':
