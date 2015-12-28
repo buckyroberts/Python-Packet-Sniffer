d6413b812b748c1605a0b880bbf8f05963aac4b7
Bucky Roberts <buckyroberts@gmail.com>
2015-12-28 05:19:17 -0500

    Working on documenting.

--- sniffer.py
+++ sniffer.py
@@ -66,17 +66,17 @@ def main():
             print(format_multi_line(DATA_TAB_1, data))
 
 
+# Unpacks ethernet frame
+def ethernet_frame(data):
+    dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
+    return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto), data[14:]
+
+
 # Returns MAC as string from bytes
 def get_mac_addr(bytes_addr):
     byte_str = map('{:02x}'.format, bytes_addr)
     mac_addr = ':'.join(byte_str).upper()
     return mac_addr
-
-
-# Unpacks ethernet frame
-def ethernet_frame(data):
-    dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
-    return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto), data[14:]
 
 
 # Unpacks IPv4 packet
